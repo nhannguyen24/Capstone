00049db6800846d20a9b8b83930472acf127b873
ACER <dnhan2426@gmail.com>
2023-09-10 22:14:56 +0700

    fix

--- config/ConnectionDatabase.js
+++ config/ConnectionDatabase.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('capstone_database_development', 'root', 'quan', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   logging: false,
   timezone: '+07:00',
