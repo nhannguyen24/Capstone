3a46fd02ad98d0854f9642e5c6d9016ca597ab52
ACER <dnhan2426@gmail.com>
2023-10-03 23:13:15 +0700

    payment

--- services/PaymentService.js
+++ services/PaymentService.js
@@ -9,8 +9,8 @@ const createMoMoPaymentRequest = (amounts) =>
             var requestId = partnerCode + new Date().getTime();
             var orderId = requestId;
             var orderInfo = "Pay with MoMo";
-            var redirectUrl = "https://www.google.com.vn/?hl=vi";
-            var ipnUrl = "https://www.google.com.vn/?hl=vi/momo-ipn";
+            var redirectUrl = "https://nbtour-fc9f59891cf4.herokuapp.com/api-docs/#/";
+            var ipnUrl = "https://nbtour-fc9f59891cf4.herokuapp.com/api/v1/payments/momo-ipn";
             // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
             var amount = amounts;
             var requestType = "captureWallet"
