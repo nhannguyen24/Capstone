d9b70f625fe2b83c3b87f05c4c9faa26998ff6b1
ACER <dnhan2426@gmail.com>
2023-10-19 00:35:05 +0700

    add location

--- services/PaymentService.js
+++ services/PaymentService.js
@@ -258,7 +258,6 @@ const getMoMoPaymentResponse = (req) =>
     new Promise(async (resolve, reject) => {
         try {
             const ipnData = req.body;
-            console.log(ipnData)
             const bookingId = ipnData.extraData
             if (ipnData.resultCode === 0) {
                 console.log(ipnData);
