29fbf48115c582a988c94d7e6f3f4745d68d3abb
Quan WeBu <quantmse151001@fpt.edu.vn>
2024-01-02 17:02:30 +0700

    Added delete expired otp for back ground job

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ const options = {
       { name: "Schedule" }, 
       { name: "Statistic" },
       { name: "Booking" }, 
-      { name: "Template" }, 
       { name: "OTP" }, 
       { name: "Payment" }, 
       { name: "Transaction" }, 
@@ -76,11 +75,11 @@ const start = () => {
   try {
     app.listen(PORT, () => {
       console.log(`Server is listening on ${PORT}...`);
-      // cron.schedule('*/30 * * * *', () => {
-      //   deleteExpiredOtp()
-      //   deleteUnPaidBooking()
-      //   rejectForm()
-      // })
+      cron.schedule('*/30 * * * *', () => {
+        deleteExpiredOtp()
+        // deleteUnPaidBooking()
+        // rejectForm()
+      })
       // cron.schedule('00 00,00 * * *', () => {
       //   cancelTourAndRefundIfUnderbooked()
       // })
